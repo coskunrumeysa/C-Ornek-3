60e57927fa03c16eb3dac28f124ae1c4e92d1b78
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2024-10-03 10:14:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Örnek_Proje_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {   //Definition Variables
+            string ilkisim;
+            string ortaisim;
+            string sonisim;
+            string tümisim;
+
+
+            // Get name functions
+            Console.Write("İlk İsmi Giriniz :");
+            ilkisim = Console.ReadLine();
+
+            Console.Write("Orta İsmi Giriniz :");
+            ortaisim = Console.ReadLine();
+
+            Console.Write("Sonuncu İsmi Giriniz:");
+            sonisim = Console.ReadLine();
+
+
+            tümisim = ilkisim +" "+ortaisim +" "+sonisim;
+
+            //Print the screen Names
+            Console.Write("İsimler :" + tümisim);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
